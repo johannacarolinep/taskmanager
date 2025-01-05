8530e02533e5d5c38349e59ec7de31ee3fc9c20a
johannacarolinep <139339346+johannacarolinep@users.noreply.github.com>
2025-01-05 12:53:03 +0100

    feat: Add styles for validation errors

--- TaskManager/wwwroot/js/site.js
+++ TaskManager/wwwroot/js/site.js
@@ -2,12 +2,39 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
-
-
 // Store the original image source
-const originalImageSrc = document.getElementById('profileImagePreview').src;
+const profileImagePreview = document.getElementById('profileImagePreview');
+const originalImageSrc = profileImagePreview ? profileImagePreview.src : null;
+
+document.addEventListener('DOMContentLoaded', () => {
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+    showAccordionErrors();
+
+});
+
+function showAccordionErrors() {
+    document.querySelectorAll('.accordion-item').forEach((accordionItem) => {
+        console.log("Found accordion item");
+        // Check if there are validation errors inside the accordion item's form
+        if (accordionItem.querySelector('.field-validation-error')) {
+            console.log("Found accordion item with error");
+            // Get the accordion header button
+            const accordionButton = accordionItem.querySelector('.accordion-button');
+            const accordionCollapse = accordionItem.querySelector('.accordion-collapse');
+
+            // Open the accordion item
+            accordionButton.classList.remove('collapsed');
+            accordionButton.setAttribute('aria-expanded', 'true');
+            accordionCollapse.classList.add('show');
+
+            // Style the accordion header to indicate an error
+            const errorIcon = `<i class="fa-solid fa-triangle-exclamation me-2"></i>`;
+            accordionButton.innerHTML = errorIcon + accordionButton.innerHTML;
+        }
+    });
+}
 
 function previewImage(event) {
     const imagePreview = document.getElementById('profileImagePreview');
