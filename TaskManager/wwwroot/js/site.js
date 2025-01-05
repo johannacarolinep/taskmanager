c6a5c690c33a589f8f2a4d1d3fb4f90fc61bd779
johannacarolinep <139339346+johannacarolinep@users.noreply.github.com>
2025-01-05 11:14:55 +0100

    feat: Add preview for image upload in AccountCenter

--- TaskManager/wwwroot/js/site.js
+++ TaskManager/wwwroot/js/site.js
@@ -4,3 +4,22 @@
 // Write your JavaScript code.
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+
+// Store the original image source
+const originalImageSrc = document.getElementById('profileImagePreview').src;
+
+function previewImage(event) {
+    const imagePreview = document.getElementById('profileImagePreview');
+    const file = event.target.files[0];
+
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function (e) {
+            imagePreview.src = e.target.result; // Set the preview image to the selected file
+        };
+        reader.readAsDataURL(file);
+    } else {
+        imagePreview.src = originalImageSrc; // Reset to original image if no file is chosen
+    }
+}
